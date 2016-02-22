24d613fb574288b6e9d8fbbf02459c6686fc4746
Bodie Shane <bodieshane68@gmail.com>
2016-02-22 09:31:35 -0700

    added the .cpp

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -16,3 +16,65 @@ CTECList::~CTECList() {
 	// TODO Auto-generated destructor stub
 }
 
+template <class Type>
+int CTECList::getSize()
+{
+
+}
+template <class Type>
+void CTECList::addToFront(Type value)
+{
+
+}
+template <class Type>
+void CTECList::addToEnd(Type value)
+{
+
+}
+template <class Type>
+void CTECList::addAtIndex(int index, Type value)
+{
+
+}
+
+template <class Type>
+Type CTECList::getFront()
+{
+
+}
+
+template <class Type>
+Type CTECList::getEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList::getFromIdex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList::removeFromFront()
+{
+
+}
+
+template <class Type>
+Type CTECList::reoveFromEnd()
+{
+
+}
+
+template <class Type>
+Type CTECList::removeFromIndex(int index)
+{
+
+}
+
+template <class Type>
+Type CTECList::set(int index, Type value)
+{
+
+}
